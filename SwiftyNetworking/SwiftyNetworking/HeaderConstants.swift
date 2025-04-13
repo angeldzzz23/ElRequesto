4ba22ec0c09f071885832f8fc0581def20ba7743
angel zambrano <angeldzambrano99@gmail.com>
2025-04-12 20:36:32 -0700

    added HeaderConstants

---
+++ SwiftyNetworking/SwiftyNetworking/HeaderConstants.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderConstants.swift
+//  SwiftyNetworking
+//
+//  Created by angel zambrano on 4/12/25.
+//
+
+import Foundation
+
+enum HeaderConstants {
+    // Standard headers
+    public static let accept = "accept"
+    public static let contentType = "Content-Type"
+    public static let userAgent = "user-agent"
+    
+    // Auth headers
+    public static let authorization = "authorization"
+    public static let sessionId = "session-id"
+    
+    // Header values
+    public static let applicationJson = "application/json"
+}
+
