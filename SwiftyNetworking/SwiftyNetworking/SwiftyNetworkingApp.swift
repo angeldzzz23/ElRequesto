701b8886f9de10e5f37ce3753779c39b72b65259
angel zambrano <angeldzambrano99@gmail.com>
2025-04-12 20:32:38 -0700

    fresh out the boat

---
+++ SwiftyNetworking/SwiftyNetworking/SwiftyNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyNetworkingApp.swift
+//  SwiftyNetworking
+//
+//  Created by angel zambrano on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
